535c69016b28848892f5c294dceb6bb153cd709e
Onur Uyar <me@onuruyar.com>
2014-10-29 11:10:53 +0000

    locale support added, data_bind added

--- Gemfile
+++ Gemfile
@@ -3,9 +3,12 @@ source 'https://rubygems.org'
 
 ruby '2.1.2'
 
-gem 'actn-db', git: "https://github.com/hackberry-gh/actn-db" #path: '../../lib/db'
-gem 'actn-jobs', git: "https://github.com/hackberry-gh/actn-jobs" #path: '../../lib/jobs'
-gem 'actn-api', git: "https://github.com/hackberry-gh/actn-api" #path: '../../lib/api'
+# gem 'actn-db', path: '../../lib/db'
+gem 'actn-db', git: "https://github.com/hackberry-gh/actn-db" 
+# gem 'actn-jobs', path: '../../lib/jobs'
+gem 'actn-jobs', git: "https://github.com/hackberry-gh/actn-jobs"
+# gem 'actn-api', path: '../../lib/api'
+gem 'actn-api', git: "https://github.com/hackberry-gh/actn-api"
 
 gem 'goliath_rack_sprockets', git: 'https://github.com/lemmycaution/goliath_rack_sprockets'  
 gem "coffee-script", "~> 2.2"
--- apis/render.rb
+++ apis/render.rb
@@ -8,18 +8,55 @@ class Render < Actn::Api::UI
     
   settings[:public_folder]  = "#{Actn::Api.root}/public"
   settings[:views_folder]   = "#{Actn::Api.root}/views"        
-  INDEX = "index.html"
+  
+  helpers do
+    
+    def locale
+      env['locale'] ||= I18n.locale.to_s
+    end
+    
+    def set_locale locale
+      # if I18n.available_locales.include?(locale.to_sym)
+        I18n.locale = locale.to_sym
+      # end
+    end
+  
+    def route path
+      filename = path[1..-1] # remove first /
+      if filename == ""
+        filename = "index.html"       
+      elsif STATICS.include? filename
+        File.read "#{Actn::Api.root}/public/#{filename}"
+      else  
+        filename = "#{filename}.html" unless filename.include?(".")
+      end
+      puts "FILENAME #{filename}"
+      begin
+        extname = File.extname(filename)[1..-1].to_sym
+        content_type extname
+        erb filename
+      rescue Errno::ENOENT => e
+        status 404
+      end
+    end
+  end
+  
+  get "/:locale/*", :locale => /^([a-z]{2}|[a-z]{2}\-[A-Z]{2})\// do
+    set_locale params[:locale].gsub(/\/$/,'')
+    path = env['REQUEST_PATH'].gsub(/\/#{params[:locale]}/,'/')
+    puts "PATH #{path.inspect}"
+    route path
+  end
+  
+  get "/:locale", :locale => /^([a-z]{2}|[a-z]{2}\-[A-Z]{2})$/ do
+    set_locale params[:locale]
+    route "/"
+  end
   
   get "/*" do
-    filename = env['REQUEST_PATH'][1..-1]
-    if filename == ""
-      filename = INDEX       
-    elsif STATICS.include? filename
-      "#{Actn::Api.root}/public/#{filename}"
-    else  
-      filename = "#{filename}.html" unless filename.include?(".")
-    end
-    erb filename
+    route env['REQUEST_PATH']
   end
+  
+  
   
 end
